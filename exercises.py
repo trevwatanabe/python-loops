7531f50bb22367aa793c8403f6d63f40ff3ed1b2
madycakes <viclee02@gmail.com>
2018-05-15 13:51:26 -1000

    updates exercises

--- exercises.py
+++ exercises.py
@@ -51,8 +51,17 @@ while i < 9:
 cars = ['Toyota', 'Tesla', 'BMW', 'Porche', 'Mercedes'] 
 for car in cars:
     print('My next car will be a red ' + car + '.')
+
+#5 Uber This Again
+#Print each item in the above cars list using a while loop.
+
+i = 0
+while i < len(cars):
+    print(cars[i])
+    i += 1
+
     
-#5  No More Tears
+#6  No More Tears
 # Create a for loop that will iterate through the cyber attacks list and prints the following: 
 #The attack at 0 is Wannacry.
 #The attack at 1 is Petya.
@@ -65,7 +74,7 @@ cyber_attacks = ['Wannacry', 'Petya', 'Locky', 'Krack Attack', 'Sambacry']
 for attack in cyber_attacks:
      print('The attack at ' + str(cyber_attacks.index(attack)) + ' is ' + attack + '.')
 
-#6 Even
+#7 Even
 # Declare a variable named even_list and assign it an empty list. Next, write a for loop that will place 25 even numbers starting from 0 into the even_list list. Print the even_list variable to see your results. 
 
 even_list = []
@@ -74,10 +83,11 @@ for number in range(0, 51):
         even_list.append(number)
 print(even_list)
 
-#7 Sum Up
+#8 Sum Up
 # Create a function named add_up which takes a parameter num. In the code block inside the function, create a variable named sum and assign it a number value of 0. Next, create a for loop that will iterate through a list of numbers using the range function that will be determined by the num parameter and will sum up all the numbers in the list and store it to the sum variable. Print the sum variable to see your results.
 
 #i.e a number list of 10 will have a sum total of 45
+
 def add_up(num):
     sum = 0
     for number in range(num):
@@ -85,7 +95,7 @@ def add_up(num):
     print sum
 add_up(10)
 
-#8 East Coast vs West Coast - A Hip Hop Rivalry
+#9 East Coast vs West Coast - A Hip Hop Rivalry
 #The East Coast - West Coast hip hop rivalry was a feud between artist and fans of the East Coast hip hop and West Coast hip hop scenes from the mid to last 1990s. 
 
 #Your job is to create a function that will loop through the rappers list and place all the odd indexed items in a list named weessst_side and all the even indexed items in a list named east_side. Print your results.
@@ -104,7 +114,26 @@ rivalry(rappers)
 print(weessst_siiide)
 print(east_side)
 
-#9 Fizz Buzz!
+
+#10 Breaking Up is Easy
+#Create a for loop that will iterate through 10 even numbers (starting from 0) and stop printing at 10.
+
+for number in range(0, 20, 2):
+    if number == 10:
+        break
+    print(number)
+
+#11 Keep Going!
+#Create a for loop that will iterate through the zip codes list below and print all the zip codes except for 96822.
+
+zip_codes = [90001,90002,90003,90004,90005,96822,90007,90008,90010,90011,90012,90013,90014,90015, 90016,90017,90018,90019]
+
+for code in zip_codes:
+    if code == 96822:
+        continue
+    print(code)
+
+#12 Fizz Buzz!
 #The classic programming task is back! Use a for loop that will iterate through 100 numbers starting from 1. Your job is to program the following:
 
 #  a) if the number is a multiple of 3, it should print 'Fizz'
@@ -134,16 +163,37 @@ for num in range(1, 101):
     else:
         print(str(num))
 
+#13 Fizz Buzz Again 
+#Do the same thing again using a while loop.
 
-#10 Roulette
+count = 0
+while count < 101:
+    if count % 5 == 0 and count % 3 == 0:
+        print('Fizz Buzz')
+    elif count % 3 == 0:
+        print('Fizz')
+    elif count % 5 == 0:
+        print('Buzz')
+    else:
+        print(count)
+    count +=1
+
+
+
+
+
+
+
+#12 Roulette
 # From wikipedia https://en.wikipedia.org/wiki/Roulette
 
 import random
-for num in range(5):
-    if random.randint(0, 37)%2 == 0 and random.randint(0, 37) == 0:
-        print(' 0 Green Color')
-    elif random.randint(0, 37)%2 == 0 :
-        print(' Even Number Black')
-    else:
-        print( ' Odd Number Red')
+
+# for num in range(5):
+#     if random.randint(0, 37)%2 == 0 and random.randint(0, 37) == 0:
+#         print(' 0 Green Color')
+#     elif random.randint(0, 37)%2 == 0 :
+#         print(' Even Number Black')
+#     else:
+#         print( ' Odd Number Red')
         
